046682e61b04ca46261d830004108b7f54bb1c5c
saka1029 <saka1029@gmail.com>
2022-05-02 13:51:26 +0900

    Jav改善。

--- src/main/java/saka1029/util/main/Jav.java
+++ src/main/java/saka1029/util/main/Jav.java
@@ -10,10 +10,29 @@ import org.jsoup.select.Elements;
 
 public class Jav {
 
+    static final int DEFAULT_MAX = 20;
+
+    static void usage() {
+        System.err.println("usage: java saka1029.util.main.Jav [-m max] KEYWORD...");
+        System.exit(1);
+    }
+
     public static void main(String[] args) throws MalformedURLException, IOException {
-        for (String arg : args)
-            get(arg);
-        
+        int max = DEFAULT_MAX;
+        int len = args.length;
+        int i = 0;
+        for ( ; i < len; ++i)
+            if (args[i].equals("-m"))
+                if (++i < len)
+                    max = Integer.parseInt(args[i]);
+                else
+                    usage();
+            else
+                break;
+        if (i >= len)
+            usage();
+        for ( ; i < len; ++i)
+            get(args[i], max);
     }
 
     public void test() throws MalformedURLException, IOException {
@@ -54,29 +73,35 @@ public class Jav {
     }
 
     static void get(String name) throws MalformedURLException, IOException {
+        get(name, DEFAULT_MAX);
+    }
+
+    static void get(String name, int max) throws MalformedURLException, IOException {
         System.out.println("#### " + name);
         String base = "https://sukebei.nyaa.si/?f=0&c=0_0&q=";
-        int max = 20;
-        get(base + name, max);
+        // int max = 20;
+        query(base + name, max);
         System.out.println();
     }
 
-    static void get(String url, int max) throws IOException {
+    static void query(String url, int max) throws IOException {
         Document doc = Jsoup.connect(url)
             .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
 //            .referrer("http://www.google.com")
             .execute()
             .parse();
         Elements table = doc.select("table.torrent-list tr");
+        System.out.println("サイズ,日付,シード数,URL,タイトル");
         int i = 0;
         for (Element e : table) {
             if (e.select("td:eq(1)").text().equals("")) continue;
             if (i++ >= max) break;
-            System.out.printf("%s,%s,%s,%s%n",
-                e.select("td:eq(1) > a").text().replaceAll("^\\W+|\\]", ""),
+            System.out.printf("%s,%s,%s,%s,%s%n",
                 e.select("td:eq(3)").text(),
                 e.select("td:eq(4)").text(),
-                e.select("td:eq(2) a:eq(0)").attr("abs:href")); // abs:は絶対URLの取得
+                e.select("td:eq(5)").text(),
+                e.select("td:eq(2) a:eq(0)").attr("abs:href"), // abs:は絶対URLの取得
+                e.select("td:eq(1) > a").text().replaceAll("^\\W+|\\]", ""));
         }
     }
 }
