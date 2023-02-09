cf682e83f1f4ff9c4f9cf289fe4c9df3726d8d99
saka1029 <saka1029@gmail.com>
2023-02-09 22:15:22 +0900

    history -> mine

--- bin/drives.bat
+++ bin/drives.bat
@@ -1,6 +1,6 @@
 setlocal
 set BASE=%USERPROFILE%\git\util
-set T=%USERPROFILE%\git\history\drives
+set T=%USERPROFILE%\git\mine\drives
 set CP=%BASE%\target\util-1.0.jar;%BASE%\lib\*
 java -cp %CP% saka1029.util.main.Drives -t %T% %*
 endlocal
