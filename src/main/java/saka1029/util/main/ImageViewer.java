47e6ec0dbb9d5f626d8529fb9e421ab2da3255cc
saka1029 <saka1029@gmail.com>
2023-02-14 14:07:27 +0900

    Update ImageViewer.java

--- src/main/java/saka1029/util/main/ImageViewer.java
+++ src/main/java/saka1029/util/main/ImageViewer.java
@@ -1,37 +1,116 @@
 package saka1029.util.main;
 
+import java.awt.BorderLayout;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
-import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class ImageViewer extends JFrame {
 
+    File file = new File("data/bike.png");
+    JLabel label = new JLabel();
+    Image image;
 
-    ImageViewer() {
-        this.setSize(1200, 600);
-        this.setTitle("ImageViewer");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ImageIcon pic = new ImageIcon("data/bike.png");// important
-        Image image = pic.getImage(); // transform it 
-//        Image resized = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-        Image resized = getScaledImage(image, this.getWidth());
-        JLabel piclabel = new JLabel(new ImageIcon(resized));
-        JPanel panel = new JPanel();
-        panel.add(piclabel);// important
-        this.add(panel);
-        this.setVisible(true);
+    void readImage() {
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    JPanel panel = new JPanel() {
+        @Override
+        public void paint(Graphics g) {
+            Image scaled = getScaledImage(image, this.getWidth());
+            g.drawImage(scaled, 0, 0, null);
+        }
+    };
     
-    private Image getScaledImage(Image src, int w){
-        double scale = (double)w / src.getWidth(null);
-        int h = (int)(src.getHeight(null) * scale);
+    final File dir;
+    final File[] files;
+    int index;
+
+    ImageViewer(File file) {
+        dir = file.getParentFile();
+        files = dir.listFiles();
+        for (index = 0; index < files.length; ++index)
+            if (files[index].equals(file))
+                break;
+        if (index >= files.length)
+            throw new IllegalArgumentException("ファイル(" + file + ")がディレクトリ(" + dir + ")にありません。");
+        setSize(1600, 900);
+        setTitle("ImageViewer");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        readImage();
+        // ボーダーレイアウト
+        setLayout(new BorderLayout());
+        getContentPane().add(panel, BorderLayout.CENTER);
+        getContentPane().add(label, BorderLayout.SOUTH);
+        JFrame f = this;
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                label.setText(e.toString());
+                switch (e.getKeyCode()) {
+                    case 27: // ESC:プログラム終了
+                        f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+                    case 'F':
+                        if (f.getExtendedState() == MAXIMIZED_BOTH) {
+                            // 通常画面表示
+                            f.setExtendedState(NORMAL);
+//                            f.setUndecorated(false); // フレームのコンストラクタで呼び出す必要がある。
+                        } else {
+                            // 全画面表示
+                            f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+//                            f.setUndecorated(true); // フレームのコンストラクタで呼び出す必要がある。
+                        }
+                        break;
+                }
+            }
+        });
+        // 全画面表示
+//        setExtendedState(JFrame.MAXIMIZED_BOTH); 
+//        setUndecorated(true);
+        // Look and feel 変更（ただし効かない）
+//        String lafClassName = "javax.swing.plaf.metal.MetalLookAndFeel";
+//        try {
+//            UIManager.setLookAndFeel(lafClassName);
+//            SwingUtilities.updateComponentTreeUI(this);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            System.exit(ABORT);
+//        }
+
+        setVisible(true);
+    }
+
+    static Image getScaledImage(Image src, int w) {
+        double scale = (double) w / src.getWidth(null);
+        int h = (int) (src.getHeight(null) * scale);
         BufferedImage resized = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = resized.createGraphics();
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
@@ -41,6 +120,11 @@ public class ImageViewer extends JFrame {
     }
 
     public static void main(String[] args) {
-        new ImageViewer();
+        if (args.length != 1)
+            throw new IllegalArgumentException("イメージファイルを指定してください。");
+        File file = new File(args[0]);
+        if (!file.exists())
+            throw new IllegalArgumentException("ファイル(" + file + ")は存在しません。");
+        new ImageViewer(file);
     }
 }
