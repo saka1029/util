58359095f92c71d0a3148be7f47e427e30d18db6
saka1029 <saka1029@gmail.com>
2023-02-16 16:51:56 +0900

    フォルダ整理。

--- makeIv.bat
+++ makeIv.bat
@@ -1,12 +1,16 @@
+::: ImageViewerを実行するためのJarファイルを作成し、
+::: exewrapを使用してExeファイルに変換します。
+::: このバッチファイルはmvn package実行後に実行する必要があります。
 setlocal
 set JAR=iv.jar
+set EXE=bin\IV.exe
 set CD=%CD%
 set JARPATH=%CD%\%JAR
 set DIR=target\classes
-set FILES=saka1029/util/main/ImageViewer*
+set CLASSES=saka1029\util\main\ImageViewer*
 set MAIN=saka1029.util.main.ImageViewer
 pushd %DIR%
-jar cvfe %JARPATH% %MAIN% %FILES%
+jar cvfe %JARPATH% %MAIN% %CLASSES%
 popd
-exewrap -g %JAR%
+exewrap -g -o %EXE% %JAR%
 endlocal
