9e8f1ebb05f8bf94ce61d2a686ee1ce04de624c7
saka1029 <saka1029@gmail.com>
2023-02-28 19:06:58 +0900

    pass mark update

--- src/main/java/saka1029/util/passmark/TestPassMark.java
+++ src/main/java/saka1029/util/passmark/TestPassMark.java
@@ -4,6 +4,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -28,17 +30,23 @@ public class TestPassMark {
         String[] ranges = {"high", "mid", "midlow", "low" };
         try (PrintWriter w = new PrintWriter(new FileWriter("data/passmaprk.csv"))) {
             w.printf("%s,%s,%s,%s,%s%n", "range", "url", "name", "passmark", "price");
+            Set<String> names = new HashSet<>();
             for (int i = 0; i < urls.length; i++) {
                 Document doc = Jsoup.parse(new URL(urls[i]), 5000);
                 Elements list = doc.select("ul.chartlist > li");
                 for (Element e : list) {
-                    String href = e.select("a").attr("abs:href");
                     String name = e.select("span.prdname").text().replace(",", " ");
+                    if (names.contains(name))
+                        continue;
+                    names.add(name);
+                    String href = e.select("a").attr("abs:href");
                     String count = e.select("span.count").text().replace(",", "");
-                    if (count.equals("NA"))
-                        count = e.select("span.mark-neww").text().replace(",", "");
-//                    String mark = e.select("span.mark-neww").text().replace(",", "");
+                    String mark = e.select("span.mark-neww").text().replace(",", "");
+                    if (!mark.isEmpty())
+                        count = mark;
                     String price = e.select("span.price-neww").text().replace(",", "");
+//                    if (count.contains("."))
+//                        System.out.println("###" + e);
                     w.printf("%s,%s,%s,%s,%s%n", ranges[i], href, name, count, price);
                 }
             }
