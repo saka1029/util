6ee63f2e2d48fe66bb77d1a22aa28d4737767ca2
saka1029 <saka1029@gmail.com>
2024-02-25 19:27:43 +0900

    drivetree.bat追加。

--- bin/drivetree.bat
+++ bin/drivetree.bat
@@ -1,6 +1,5 @@
 setlocal
 set BASE=%USERPROFILE%\git\util
-set T=\\minipc\d\git\mine\drives
 set CP=%BASE%\target\util-1.0-jar-with-dependencies.jar
-java -cp %CP% saka1029.util.main.Drive %T% %*
+java -cp %CP% saka1029.util.main.DriveTree %*
 endlocal
