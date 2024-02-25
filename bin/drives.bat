31b2ec2fbb9a2e594eb780ca0f84e2f50eff262d
saka1029 <saka1029@gmail.com>
2024-02-26 07:32:36 +0900

    Drivesクラス削除。

--- bin/drives.bat
+++
@@ -1,6 +0,0 @@
-setlocal
-set BASE=%USERPROFILE%\git\util
-set T=\\minipc\d\git\mine\drives
-set CP=%BASE%\target\util-1.0-jar-with-dependencies.jar
-java -cp %CP% saka1029.util.main.Drives -t %T% %*
-endlocal
