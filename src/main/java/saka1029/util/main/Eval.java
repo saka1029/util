edaa38709f8be719cd8425b2034d2b7deb21a04c
saka1029 <saka1029@gmail.com>
2024-03-02 15:36:55 +0900

    main追加。

---
+++ src/main/java/saka1029/util/main/Eval.java
@@ -0,0 +1,19 @@
+package saka1029.util.main;
+
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+import saka1029.util.eval.Parser;
+
+public class Eval {
+
+    static void run(Reader reader, Writer writer) {
+        PrintWriter out = new PrintWriter(writer);
+
+    }
+
+    public static void main(String[] args) {
+        run()
+    }
+    
+}
