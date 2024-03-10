9a6e37b7b0cc5ff230234b1234eec34f8ecc248f
saka1029 <saka1029@gmail.com>
2024-03-10 22:31:50 +0900

    ..

---
+++ src/main/java/saka1029/util/vector/Lexer.java
@@ -0,0 +1,121 @@
+package saka1029.util.vector;
+
+import java.math.BigDecimal;
+
+public class Lexer {
+    public record Token(int type, String string) {
+        public BigDecimal number() {
+            return new BigDecimal(string);
+        }
+        @Override
+        public final String toString() {
+            return "%c%s".formatted((char)type, string == null ? "" : ":" + string);
+        }
+    }
+
+    final int[] input;
+    int index;
+    int ch;
+
+    Lexer(String input) {
+        this.input = input.codePoints().toArray();
+        this.index = 0;
+        get();
+    }
+
+    public static Lexer of(String input) {
+        return new Lexer(input);
+    }
+
+    int get() {
+        return ch = index < input.length ? input[index++] : -1;
+    }
+
+    void spaces() {
+        while (Character.isWhitespace(ch))
+            get();
+    }
+
+    static boolean isDigit(int ch) {
+        return ch >= '0' && ch <= '9';
+    }
+
+    static boolean isIdFirst(int ch) {
+        return Character.isAlphabetic(ch) || ch == '_';
+    }
+
+    static boolean isIdRest(int ch) {
+        return isIdFirst(ch) || Character.isDigit(ch);
+    }
+
+    StringBuilder sb = new StringBuilder();
+
+    void clear() {
+        sb.setLength(0);
+    }
+
+    void append(int ch) {
+        sb.appendCodePoint(ch);
+    }
+
+    void appendGet() {
+        append(ch);
+        get();
+    }
+
+    void digit() {
+        if (!isDigit(ch))
+            throw new VectorException("Digit expected");
+        while (isDigit(ch))
+            appendGet();
+    }
+
+    Token number() {
+        clear();
+        digit();
+        if (ch == '.') {
+            appendGet();
+            digit();
+        }
+        if (ch == 'e' || ch == 'E') {
+            appendGet();
+            if (ch == '+' || ch == '-')
+                appendGet();
+            digit();
+        }
+        return new Token('n', sb.toString());
+    }
+
+    Token id() {
+        clear();
+        while (isIdRest(ch))
+            appendGet();
+        return new Token('i', sb.toString());
+    }
+
+    public Token read() {
+        spaces();
+        if (ch == -1)
+            return null;
+        switch (ch) {
+            case '(':
+            case ')':
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+            case '^':
+                int t = ch;
+                get();
+                return new Token(t, null);
+            default:
+                if (isDigit(ch))
+                    return number();
+                else if (isIdFirst(ch))
+                    return id();
+                else
+                    throw new VectorException("Unknown char 0x%02X", ch);
+        }
+    }
+}
--- src/test/java/test/saka1029/util/vector/TestLexer.java
+++ src/test/java/test/saka1029/util/vector/TestLexer.java
@@ -1,8 +1,11 @@
 package test.saka1029.util.vector;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import saka1029.util.vector.Lexer;
+import saka1029.util.vector.Lexer.Token;
 
 public class TestLexer {
 
@@ -20,9 +23,27 @@ public class TestLexer {
         assertTrue(Character.isAlphabetic('あ'));
         assertTrue(Character.isAlphabetic('漢'));
         assertTrue(Character.isAlphabetic('三'));
+        assertTrue(Character.isAlphabetic(0x29e3d)); // 𩸽
         assertFalse(Character.isAlphabetic('3'));
         assertFalse(Character.isAlphabetic('３'));
         assertFalse(Character.isAlphabetic('．'));
+    }
+
+    static String lex(String input) {
+        Lexer l = Lexer.of(input);
+        StringBuilder sb = new StringBuilder();
+        Token t;
+        while ((t = l.read()) != null)
+            sb.append(" ").append(t);
+        return sb.substring(1);
+    }
+
+    @Test
+    public void testRead() {
+        assertEquals("( n:1 + n:234 )", lex("  ( 1 + 234 )"));
+        assertEquals("n:234.30e-3", lex("234.30e-3"));
+        assertEquals("+ - * / % ^", lex("+-*/%^"));
+        assertEquals("i:漢字 i:𩸽２", lex(" 漢字  𩸽２  "));
     }
 
 }
