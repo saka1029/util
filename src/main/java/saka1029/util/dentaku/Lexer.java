df36d5bf7399522563b12418c26ce219d9097daf
saka1029 <saka1029@gmail.com>
2024-04-04 17:15:28 +0900

    Lexerテスト

--- src/main/java/saka1029/util/dentaku/Lexer.java
+++ src/main/java/saka1029/util/dentaku/Lexer.java
@@ -3,6 +3,8 @@ package saka1029.util.dentaku;
 import java.util.ArrayList;
 import java.util.List;
 
+import saka1029.util.dentaku.Tokenizer.Type;
+
 public class Lexer {
 
     public enum Type {
@@ -15,7 +17,7 @@ public class Lexer {
     public static final Token END = new Token(Type.END, "");
 
     final int[] input;
-    int ch, index, start;
+    int ch, current, index, start;
 
     Lexer(int[] input) {
         this.input = input;
@@ -33,7 +35,20 @@ public class Lexer {
     }
 
     int get() {
+        current = index;
         return ch = index < input.length ? input[index++] : -1;
+    }
+
+    static boolean isDigit(int ch) {
+        return ch >= '0' && ch <= '9';
+    }
+
+    static boolean isIdFirst(int ch) {
+        return Character.isAlphabetic(ch) || ch == '_';
+    }
+
+    static boolean isIdRest(int ch) {
+        return isIdFirst(ch) || Character.isDigit(ch) || ch == '-' || ch == '.';
     }
 
     Type getReturn(Type type) {
@@ -41,17 +56,65 @@ public class Lexer {
         return type;
     }
 
+    void digit() {
+        if (!isDigit(ch))
+            throw new ValueException("Digit expected but 0x%04X", ch);
+        while (isDigit(ch))
+            get();
+    }
+
+    Type number() {
+        digit();
+        if (ch == '.') {
+            get();
+            digit();
+        }
+        if (ch == 'e' || ch == 'E') {
+            get();
+            if (ch == '+' | ch == '-')
+                get();
+            digit();
+        }
+        return Type.NUMBER;
+    }
+
+    Type id() {
+        while (isIdRest(ch))
+            get();
+        return Type.ID;
+    }
+
     Token token() {
         while (Character.isWhitespace(ch))
             get();
-        int start = index - 1;
+        int start = current;
+        if (ch == -1)
+            return END;
         Type type = switch (ch) {
-            case -1 -> Type.END;
-            case '+', '-', '*', '/', '%', '^' -> getReturn(Type.OTHER);
-            default -> throw new ValueException("Unknown char 0x%04x", ch);
+            case '(', ')', '+', '*', '/', '%', '^', '~' -> getReturn(Type.OTHER);
+            case '-' -> isDigit(get()) ? number() : Type.OTHER;
+            case '=' -> get() == '=' ? getReturn(Type.OTHER) : Type.OTHER;
+            case '<' -> get() == '=' ? getReturn(Type.OTHER) : Type.OTHER;
+            case '>' -> get() == '=' ? getReturn(Type.OTHER) : Type.OTHER;
+            case '!' -> {
+                get();
+                if (ch == '=')
+                    yield getReturn(Type.OTHER);
+                else if (ch == '~')
+                    yield getReturn(Type.OTHER);
+                else
+                    throw new ValueException("Unknown char after '!' 0x04X", ch);
+            }
+            default -> {
+                if (isDigit(ch))
+                    yield number();
+                else if (isIdFirst(ch))
+                    yield id();
+                else
+                    throw new ValueException("Unknown char 0x%04x", ch);
+            }
         };
-        int end = index - (index < input.length ? 1 : 0);
-        return new Token(type, new String(input, start, end - start));
+        return new Token(type, new String(input, start, current - start));
     }
     
 }
--- src/test/java/test/saka1029/dentaku/TestLexer.java
+++ src/test/java/test/saka1029/dentaku/TestLexer.java
@@ -12,5 +12,41 @@ public class TestLexer {
     @Test
     public void testTokens() {
         assertEquals(List.of(new Token(Type.OTHER, "+")), Lexer.tokens("  +"));
+        assertEquals(List.of(
+            new Token(Type.OTHER, "("),
+            new Token(Type.OTHER, "+"),
+            new Token(Type.OTHER, ")")
+        ), Lexer.tokens("(+)"));
+        assertEquals(List.of(new Token(Type.NUMBER, "-123.45e2")), Lexer.tokens("  -123.45e2"));
+        assertEquals(List.of(new Token(Type.OTHER, "-")), Lexer.tokens("  -"));
+        assertEquals(List.of(
+            new Token(Type.ID, "aB2"),
+            new Token(Type.OTHER, "+")
+        ), Lexer.tokens("  aB2+ "));
+        assertEquals(List.of(
+            new Token(Type.OTHER, "<"),
+            new Token(Type.OTHER, "-")
+        ), Lexer.tokens("  <-"));
+        assertEquals(List.of(
+            new Token(Type.OTHER, "<"),
+            new Token(Type.NUMBER, "-2")
+        ), Lexer.tokens("  <-2"));
+        assertEquals(List.of(
+            new Token(Type.OTHER, "-"),
+            new Token(Type.OTHER, "-"),
+            new Token(Type.ID, "a")
+        ), Lexer.tokens("  --a"));
+        assertEquals(List.of(
+            new Token(Type.OTHER, "<="),
+            new Token(Type.OTHER, ">")
+        ), Lexer.tokens("  <=>"));
+        assertEquals(List.of(
+            new Token(Type.OTHER, ">="),
+            new Token(Type.OTHER, "<")
+        ), Lexer.tokens("  >=<"));
+        assertEquals(List.of(
+            new Token(Type.OTHER, "="),
+            new Token(Type.OTHER, "<=")
+        ), Lexer.tokens("  =<="));
     }
 }
