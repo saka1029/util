032a8e57157ba0a28711c3d43d963b41a6888f25
saka1029 <saka1029@gmail.com>
2024-07-01 17:16:37 +0900

    a

--- bin/ffdown.bat
+++ bin/ffdown.bat
@@ -6,7 +6,7 @@ setlocal
 :::     ffdown.bat 動画インデックスファイル.m3u8
 :::     カレントディレクトリに動画ファイル(.mp4)がダウンロードされます。
 if not "%~x1"==".m3u8" goto ERROR
-ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i %1 -movflags faststart -c copy %~n1.mp4
+ffmpeg -allowed_extensions ALL -protocol_whitelist file,http,https,tcp,tls,crypto -i %1 -movflags faststart -c copy %~n1.mp4
 goto END
 
 :ERROR
