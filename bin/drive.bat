489abfde7507911ef946507335b941ccfd3aa783
saka1029 <saka1029@gmail.com>
2024-11-07 14:25:05 +0900

    drive移行

--- bin/drive.bat
+++ bin/drive.bat
@@ -1,6 +1,6 @@
 setlocal
 set BASE=%USERPROFILE%\git
-set T=%BASE%\mine\drive
+set T=%BASE%\util\drive
 set CP=%BASE%\util\target\util-1.0-jar-with-dependencies.jar
 java -cp %CP% saka1029.util.main.Drive %T% %*
 endlocal
