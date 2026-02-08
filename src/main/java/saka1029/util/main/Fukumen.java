c1573141e0b7fd7c67f470f9a4cbb9b6837d03f2
saka1029 <saka1029@gmail.com>
2026-02-09 06:34:07 +0900

    ..

--- src/main/java/saka1029/util/main/Fukumen.java
+++ src/main/java/saka1029/util/main/Fukumen.java
@@ -18,24 +18,19 @@ public class Fukumen {
         return problem;
     }
 
-    static final String USAGE = """
-        usage:
-        java saka1029.util.main.Fukumen -e EXPRESSION"
-        or"
-        java saka1029.util.main.Fukumen EXPRESSION_FILE"
-        """;
+    static final String USAGE = "usage: java saka1029.util.main.Fukumen (EXPRESSION | -f EXPRESSION_FILE)";
 
     public static void main(String[] args) throws IOException,
             IllegalAccessException, InvocationTargetException,
             NoSuchMethodException, SecurityException,
             ClassNotFoundException, CompileError {
         Problem problem;
-        if (args.length == 1)
-            problem = FukumenParser.parse(Files.readString(Paths.get(args[0])));
-        else if (args.length == 2 && args[0].equals("-e"))
-            problem = FukumenParser.parse(args[1]);
+        if (args.length == 2 && args[0].equals("-f"))
+            problem = FukumenParser.parse(Files.readString(Paths.get(args[1])));
+        else if (args.length == 1)
+            problem = FukumenParser.parse(args[0]);
         else
-            throw new RuntimeException("usage: java saka1029.util.main.Fukumen FUKUMEN_FILE");
+            throw new RuntimeException(USAGE);
         problem.solve();
     }
 
