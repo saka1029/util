b09f1b709b5088f1449a2fbf655857c067d324bd
saka1029 <saka1029@gmail.com>
2026-07-13 05:19:47 +0900

    Calendar

---
+++ src/main/java/saka1029/util/calendar/Calendar.java
@@ -0,0 +1,5 @@
+package saka1029.util.calendar;
+
+public class Calendar {
+
+}
---
+++ src/test/java/test/saka1029/util/calendar/TestCalendar.java
@@ -0,0 +1,7 @@
+package test.saka1029.util.calendar;
+
+public class TestCalendar {
+
+    static String 祝日CSV = "https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv";
+
+}
